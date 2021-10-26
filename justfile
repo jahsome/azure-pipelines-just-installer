74b3cb7f70df270e12a3889319497a8aca2362fe
Jake Harris <jake@scuttlebyte.com>
2021-10-26 09:42:04 -0700

    🌱 Initial commit

---
+++ justfile
@@ -0,0 +1,34 @@
+set positional-arguments
+
+install:
+    npm install
+
+clean: 
+    rm -rf dist
+    mkdir -p dist
+
+build:
+    #!/bin/bash
+    npx tsc
+    version=$(cat package.json | jq -r '.version | split(".") | { Major: .[0], Minor: .[1], Patch: .[2]}')
+    cat src/resources/task.json | jq -r --argjson version "$version" ' .version |= $version ' > dist/task.json
+    cat src/resources/vss-extension.json | jq -r --argjson version "$version" ' .version |= $version ' > dist/vss-extension.json
+
+version version="patch":
+    npm version {{ version }}
+
+package:
+    npx tfx extension --manifest-globs dist/vss-extension.json create
+
+publish version="minor": (version version)
+    npx tfx extension \
+        --manifest-globs dist/vss-extension.json \
+        --auth-type pat \
+        --no-prompt -t $AZURE_DEVOPS_PAT \
+        -u https://marketplace.visualstudio.com \
+        publish
+
+release version="minor": install clean build package (version version)
+
+run:
+    node dist/index.js
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as os from "os";
+import * as taskLib from "azure-pipelines-task-lib/task";
+import * as toolLib from "azure-pipelines-tool-lib/tool";
+const axios = require("axios").default;
+
+async function run() {
+  const osPlat = os.platform();
+  const version = taskLib.getInput("version") ?? (await getLatestVersion());
+
+  const targets: {
+    [key: string]: string;
+  } = {
+    linux: "x86_64-unknown-linux-musl",
+    win32: "x86_64-pc-windows-msvc",
+  };
+
+  const target = targets[osPlat];
+
+  try {
+    if (!toolLib.findLocalTool("just", version)) {
+      installJust(version, target);
+    }
+  } catch (err: any) {
+    taskLib.setResult(
+      taskLib.TaskResult.Failed,
+      taskLib.loc("JustInstallerFailed", err.message)
+    );
+  }
+}
+
+async function installJust(version: string, target: string) {
+  // Construct the download URL
+  const justUrl = `https://github.com/casey/just/releases/download/${version}/just-${version}-${target}.tar.gz`;
+
+  // Download the .tar.gz and extract it
+  const temp = await toolLib.downloadTool(justUrl);
+  const extractRoot = await toolLib.extractTar(temp);
+
+  // Cache the bin
+  const cachePath = await toolLib.cacheDir(extractRoot, "just", version);
+
+  // Now prepend the tool's bin to the path
+  toolLib.prependPath(cachePath);
+}
+
+async function getLatestVersion() {
+  const res = await axios.get(
+    `https://api.github.com/repos/casey/just/releases/latest`
+  );
+
+  return res.data.tag_name;
+}
+
+run();
