c43e354b81b653366b7dc42312aa3576ab48b160
Jake Harris <jake@scuttlebyte.com>
2021-10-26 23:06:30 -0700

    Rollup cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import typescript from "@rollup/plugin-typescript"
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-
+import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'src/index.ts',
   output: {
     dir: "dist/lib",
     format: 'cjs'
   },
-  plugins: [nodeResolve(), typescript()]
+  plugins: [nodeResolve(), commonjs(), typescript()]
 }
